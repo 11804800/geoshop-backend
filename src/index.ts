11a180bb11e5669ef7e292913189654b9c28a8ea
Nikhil Pathak <nikhilpathak2602@gmail.com>
2025-11-06 11:02:59 +0530

    Cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import bodyParser from 'body-parser';
 import { configDotenv } from 'dotenv';
 configDotenv();
-
+import cors from "cors";
 import express, { Request, Response } from 'express'
 import path from 'path';
 import ProductRouter from './route/ProductRouter';
 import mongoose from 'mongoose';
 
 const MongoDB_url = process.env.MONGODB_URL;
+
+const allowedOrigins = [
+    "http://localhost:3001"
+];
+
+const corsWithOptions = {
+    origin: function (origin: any, callback: any) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
 
 if (!MongoDB_url) {
     throw new Error("Environment Variable Mongodb url is not defined");
@@ -23,7 +42,7 @@ const app: any = express();
 
 const PORT: string | number = process.env.PORT || 3000;
 
-
+app.use(cors(corsWithOptions));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(bodyParser.json());
